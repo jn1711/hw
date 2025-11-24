ba9daf685cc467afad4f35102b72fb1b0d1e45c0
jn1711 <113297233+jn1711@users.noreply.github.com>
2025-11-24 18:49:11 +0500

    ademyy

--- console/Sulifa.java
+++ console/Sulifa.java
@@ -13,49 +13,61 @@ public class Sulifa {
 
         System.out.println("==============================\nСУ ЛИ ФА ойынына қош келдіңіз!\nКанша раунд ойнаймыз?\n==============================");
         int round = sc.nextInt();
-        int [] oiyn = new int[round]; //0-утылдын 1-уттын 3-ничья 
+        int [] oiyn = new int[round]; //0-утылдын 1-уттын 3-ничья
+
         System.out.println("------------------------------\n====    Ойын басталды!    ====");
         for (int i=0; i<round; i++){
             int bot = rd.nextInt(1,4);
-            System.out.println("------------------------------\n====      1-шы раунд      ====\n------------------------------");
-            System.out.println("====      Танданыз:      ====\n1-Кагаз\n2-Кудык\n3-Кайшы\n------------------------------");
+            System.out.println("------------------------------\n====      " + (i+1) +"-ші раунд      ====\n------------------------------");
+            System.out.println("====      Танданыз:      ====\n1-Кағаз\n2-Құдық\n3-Қайшы\n------------------------------");
             int adam = sc.nextInt();
+            if (adam < 1 || adam > 3) {
+                System.out.println("Қате! 1-3 аралығында мән енгізіңіз.");
+                i--;
+                continue;
+            }
             if (adam == bot) {
-                System.out.println("Сыздын тандауыныз: " + adam + "\nБоттын тандауы: " + bot + "\nНатиже: Ничья");
+                System.out.println("------------------------------\nСіздің таңдауыңыз: " + adam + "\nБоттың таңдауы: " + bot + "\nНәтиже: Ничья");
                 oiyn[i] = 3;
             }else if (adam == 1 && bot == 2) {
                 count_adam++;
-                System.out.println("Сыздын тандауыныз: " + adam + "\nБоттын тандауы: " + bot + "\nНатиже: Сыз уттыныз");
+                System.out.println("------------------------------\nСіздің таңдауыңыз: " + adam + "\nБоттың таңдауы: " + bot + "\nНәтиже: Сіз ұттыңыз");
                 oiyn[i] = 1;
             }else if (adam == 1 && bot == 3) {
                 count_bot++;
-                System.out.println ("Сыздын тандауыныз: " + adam + "\nБоттын тандауы: " + bot + "\nНатиже: Бот утты");
+                System.out.println ("------------------------------\nСіздің таңдауыңыз: " + adam + "\nБоттың таңдауы: " + bot + "\nНәтиже: Бот ұтты");
                 oiyn[i]=0;
             }else if (adam==2 && bot == 1) {
                 count_bot++;
-                System.out.println("Сыздын тандауыныз: " + adam + "\nБоттын тандауы: " + bot + "\nНатиже: Бот утты");
+                System.out.println("------------------------------\nСіздің таңдауыңыз: " + adam + "\nБоттың таңдауы: " + bot + "\nНәтиже: Бот ұтты");
                 oiyn[i]=0;
             }else if (adam == 2 && bot == 3) {
                 count_adam++;
-                System.out.println("Сыздын тандауыныз: " + adam + "\nБоттын тандауы: " + bot + "\nНатиже: Сыз уттыныз");
+                System.out.println("------------------------------\nСіздің таңдауыңыз: " + adam + "\nБоттың таңдауы: " + bot + "\nНәтиже: Сіз ұттыңыз");
                 oiyn[i] = 1;
             }else if (adam == 3 && bot == 1) {
                 count_adam++;
-                System.out.println("Сыздын тандауыныз: " + adam + "\nБоттын тандауы: " + bot + "\nНатиже: Сыз уттыныз");
+                System.out.println("------------------------------\nСіздің таңдауыңыз: " + adam + "\nБоттың таңдауы: " + bot + "\nНәтиже: Сіз ұттыңыз");
                 oiyn[i] = 1;
             }else if (adam == 3 && bot == 2) {
                 count_bot++;
-                System.out.println("Сыздын тандауыныз: " + adam + "\nБоттын тандауы: " + bot + "\nНатиже: Бот утты");
+                System.out.println("------------------------------\nСіздің таңдауыңыз: " + adam + "\nБоттың таңдауы: " + bot + "\nНәтиже: Бот ұтты");
                 oiyn[i]=0;
-            }else {
-                System.out.println("Қате");
-                oiyn[i] = -1;
             }
         }
-            System.out.println("Ойын нәтижесі: ");
-            for(int i=0; i<oiyn.length; i++){
-                System.out.println(oiyn[i]);
+        System.out.println("------------------------------\n----    Ойын нәтижесі:    ----\n------------------------------");
+        for (int i=0; i<round; i++){
+            System.out.println("====      " + (i+1) +"-ші раунд      ====");
+            if (oiyn[i] == 3){
+                System.out.println((i+1)+". Ничья");
+            } else if (oiyn[i] == 1) {
+                System.out.println((i+1)+". Сіз ұттыңыз");
+            } else if (oiyn[i] == 0) {
+                System.out.println((i+1)+". Бот ұтты");
             }
-            System.out.println("Жалпы счет: " + count_adam + ":" + count_bot);
+        }
+        System.out.println("==============================\n====         Cчет         ====\n----     Сіз " + count_adam + "-" + count_bot + " Бот      ----");
+        System.out.println("------------------------------");
+
     }
 }
